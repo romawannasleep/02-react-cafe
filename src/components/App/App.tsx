6bbfcc2997d55eeb25b2045355ed0a728d3e9595
romawannasleep <roman.mx.man.2710@gmail.com>
2026-04-30 21:56:20 +0300

    1235

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,7 +5,7 @@ import Info from "../CafeInfo/CafeInfo";
 import Options from "../VoteOptions/VoteOptions";
 import Stats from "../VoteStats/VoteStats";
 import css from  './App.module.css';
-import Notification from "../notification/notification";
+import Notification from "../Notification/Notification";
 
 const initialState: Votes = {
 	good: 0,
